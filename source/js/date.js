07e8ef3324415d264a55977e0214899c7dbf41b9
Svetlana <se55petrova@gmail.com>
2024-09-06 18:29:17 +0500

    Настроила отрисовку данных таблицы по данным из js - заглушки

--- source/js/date.js
+++ source/js/date.js
@@ -20,3 +20,21 @@ const dateList = [
         name: 'Акционерное общество'
     }
 ]
+
+const rowTemplate = document.querySelector('#date-table').content.querySelector('.data-item');
+const dateTableContainer = document.querySelector('#date-table-container')
+
+const rowListFragment = document.createDocumentFragment()
+
+dateList.forEach((row) => {
+    const rowElement = rowTemplate.cloneNode(true);
+
+    rowElement.querySelector('#date-id').textContent = row.id;
+    rowElement.querySelector('#date-full-name').textContent = row.name;
+    rowElement.querySelector('#date-abb-name').textContent = row.abbreviation;
+
+    rowListFragment.appendChild(rowElement);
+});
+
+dateTableContainer.appendChild(rowListFragment)
+
